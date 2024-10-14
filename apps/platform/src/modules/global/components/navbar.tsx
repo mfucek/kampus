ebd81c25a47c618b2f7ffacc00cbe9f4229fbd10
Matija Fućek <matijafucek1@gmail.com>
2024-10-14 19:25:20 +0200

    add login & register page

--- apps/platform/src/modules/global/components/navbar.tsx
+++ apps/platform/src/modules/global/components/navbar.tsx
@@ -1,5 +1,6 @@
 import { Button } from '@/lib/shadcn/ui/button';
 import { useTranslation } from '@/utils/translations/use-translation';
+import Link from 'next/link';
 
 export const Navbar = () => {
 	const { t } = useTranslation('hr');
@@ -8,12 +9,16 @@ export const Navbar = () => {
 		<div className="bg-section border-b-neutral-weak h-14 border-b flex flex-row justify-between items-center px-2">
 			<div className="title-3">Kampus.hr</div>
 			<div className="flex flex-row gap-2">
-				<Button theme="accent" size="md" variant="solid-weak">
-					{t.register}
-				</Button>
-				<Button theme="accent" size="md" variant="solid">
-					{t.login}
-				</Button>
+				<Link href="/register">
+					<Button theme="accent" size="md" variant="solid-weak">
+						{t.register}
+					</Button>
+				</Link>
+				<Link href="/login">
+					<Button theme="accent" size="md" variant="solid">
+						{t.login}
+					</Button>
+				</Link>
 			</div>
 		</div>
 	);

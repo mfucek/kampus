3cb24f0f687de1a68aabea41097fc4a156ce9262
Matija Fućek <matijafucek1@gmail.com>
2024-10-15 00:02:08 +0200

    adjust page layout

--- apps/platform/src/modules/global/components/navbar.tsx
+++ apps/platform/src/modules/global/components/navbar.tsx
@@ -5,6 +5,23 @@ import Link from 'next/link';
 export const Navbar = () => {
 	const { t } = useTranslation('hr');
 
+	const isLoggedIn = true;
+
+	if (isLoggedIn) {
+		return (
+			<div className="bg-section border-b-neutral-weak h-14 border-b flex flex-row justify-between items-center px-2">
+				<div className="title-3">Kampus.hr</div>
+				<div className="flex flex-row gap-2">
+					<Link href="/home">
+						<Button theme="accent" size="md" variant="solid">
+							{t.goToPlatform}
+						</Button>
+					</Link>
+				</div>
+			</div>
+		);
+	}
+
 	return (
 		<div className="bg-section border-b-neutral-weak h-14 border-b flex flex-row justify-between items-center px-2">
 			<div className="title-3">Kampus.hr</div>

b90f2411359b0f89d11bb393fe91c678a6bb7ecd
Matija Fućek <matijafucek1@gmail.com>
2024-10-15 00:26:01 +0200

    style homepage to be responsive

--- apps/platform/src/modules/college/components/college-grid.tsx
+++ apps/platform/src/modules/college/components/college-grid.tsx
@@ -3,23 +3,36 @@
 import { api } from '@/lib/trpc/react';
 import { Container } from '@/modules/global/components/container';
 import Link from 'next/link';
+import { useState } from 'react';
 import { CollegeCard } from './college-card';
 
 export const CollegeGrid = () => {
 	const { data, isLoading } = api.college.hello.useQuery();
 
+	const [search, setSearch] = useState('');
+
 	return (
 		<Container>
 			<div className="py-10 text-center">
 				<h3>Koji faks trazis?</h3>
-				<input type="text" placeholder="Unesi fakultet" />
+				<input
+					type="text"
+					placeholder="Unesi fakultet"
+					onChange={(e) => setSearch(e.target.value)}
+				/>
 			</div>
-			<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2">
-				{data?.map((college) => (
-					<Link href={`/${college.slug}`} key={college.slug}>
-						<CollegeCard college={college} />
-					</Link>
-				))}
+			<div className="grid grid-cols-1 xs:grid-cols-2 md:grid-cols-3 gap-2">
+				{data
+					?.filter(
+						(college) =>
+							college.name.toLowerCase().includes(search.toLowerCase()) ||
+							college.slug.toLowerCase().includes(search.toLowerCase())
+					)
+					.map((college) => (
+						<Link href={`/${college.slug}`} key={college.slug}>
+							<CollegeCard college={college} />
+						</Link>
+					))}
 			</div>
 		</Container>
 	);
--- apps/platform/tailwind.config.ts
+++ apps/platform/tailwind.config.ts
@@ -64,6 +64,13 @@ export default {
 					contrast: 'var(--color-theme-contrast)'
 				}
 			}
+		},
+		screens: {
+			xs: '400px',
+			sm: '640px',
+			md: '768px',
+			lg: '1024px',
+			xl: '1280px'
 		}
 	},
 	plugins: [],

58c46c5b3f7e123dd6e0e5c6c22bb5241bbb811a
Matija Fućek <matijafucek1@gmail.com>
2024-10-22 14:21:18 +0200

    add logout to profile settings

--- apps/platform/src/modules/profile/components/sections/danger-zone.tsx
+++ apps/platform/src/modules/profile/components/sections/danger-zone.tsx
@@ -3,7 +3,7 @@ import { Button } from '@/lib/shadcn/ui/button';
 export const DangerZoneSection = () => {
 	return (
 		<div>
-			<Button theme="danger" variant="outline">
+			<Button theme="danger" variant="outline" disabled>
 				Delete Account
 			</Button>
 		</div>

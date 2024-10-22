3c5d011b6de96035b5709221825468a24b2e7955
Matija Fućek <matijafucek1@gmail.com>
2024-10-22 14:21:18 +0200

    add logout to profile settings

--- apps/platform/src/modules/profile/components/sections/account-info.tsx
+++ apps/platform/src/modules/profile/components/sections/account-info.tsx
@@ -1,18 +1,25 @@
-import { Button } from '@/lib/shadcn/ui/button';
-import { api } from '@/lib/trpc/server';
+'use client';
 
-export const AccountInfoSection = async () => {
-	const user = await api.account.getUser();
-	const account = await api.account.getAccount();
+import { Button } from '@/lib/shadcn/ui/button';
+import { api } from '@/lib/trpc/react';
+import { useAuth } from '@clerk/nextjs';
+
+export const AccountInfoSection = () => {
+	const { data: user } = api.account.getUser.useQuery();
+	const { data: account } = api.account.getAccount.useQuery();
+
+	const { signOut } = useAuth();
 
 	return (
 		<>
 			<div className="flex flex-col p-6 border border-neutral-weak rounded-xl">
 				<p className="body-3 text-neutral-strong">Account: {account?.id}</p>
-				<p className="body-3 text-neutral-strong">User: {user.id}</p>
+				<p className="body-3 text-neutral-strong">User: {user?.id}</p>
 				<p className="body-3 text-neutral-strong">Status: {account?.status}</p>
 			</div>
-			<Button variant="outline">Log out</Button>
+			<Button variant="outline" onClick={() => signOut()}>
+				Log out
+			</Button>
 		</>
 	);
 };

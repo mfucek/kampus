eaea57fd7b415448368bbc76922a3e20a65d80ab
Matija Fućek <matijafucek1@gmail.com>
2024-10-24 01:57:57 +0200

    fix up mock materials table; clean up staff router

--- apps/platform/src/modules/materials/components/materials-table.tsx
+++ apps/platform/src/modules/materials/components/materials-table.tsx
@@ -1,9 +1,7 @@
 import {
 	Table,
 	TableBody,
-	TableCaption,
 	TableCell,
-	TableFooter,
 	TableHead,
 	TableHeader,
 	TableRow
@@ -11,77 +9,59 @@ import {
 
 const invoices = [
 	{
-		invoice: 'INV001',
-		paymentStatus: 'Paid',
-		totalAmount: '$250.00',
-		paymentMethod: 'Credit Card'
+		file: 'Meduispit 2024/2025',
+		author: 'Joca',
+		tags: 'Meduispit, Kolokvij, Rijesen',
+		actions: 'Preuzmi'
 	},
 	{
-		invoice: 'INV002',
-		paymentStatus: 'Pending',
-		totalAmount: '$150.00',
-		paymentMethod: 'PayPal'
+		file: 'Zavrsni ispit 2024/2025',
+		author: 'Franko',
+		tags: 'Zavrsni ispit, Rijesen',
+		actions: 'Preuzmi'
 	},
 	{
-		invoice: 'INV003',
-		paymentStatus: 'Unpaid',
-		totalAmount: '$350.00',
-		paymentMethod: 'Bank Transfer'
+		file: 'JIR 2023/2024',
+		author: 'Marko',
+		tags: 'Rok, Rijesen',
+		actions: 'Preuzmi'
 	},
 	{
-		invoice: 'INV004',
-		paymentStatus: 'Paid',
-		totalAmount: '$450.00',
-		paymentMethod: 'Credit Card'
-	},
-	{
-		invoice: 'INV005',
-		paymentStatus: 'Paid',
-		totalAmount: '$550.00',
-		paymentMethod: 'PayPal'
-	},
-	{
-		invoice: 'INV006',
-		paymentStatus: 'Pending',
-		totalAmount: '$200.00',
-		paymentMethod: 'Bank Transfer'
-	},
-	{
-		invoice: 'INV007',
-		paymentStatus: 'Unpaid',
-		totalAmount: '$300.00',
-		paymentMethod: 'Credit Card'
+		file: 'Zavrsni ispit 2023/2024',
+		author: 'Marko',
+		tags: 'Zavrsni ispit, Rijesen',
+		actions: 'Preuzmi'
 	}
 ];
 
 export const MaterialsTable = () => {
 	return (
 		<Table>
-			<TableCaption>A list of your recent invoices.</TableCaption>
+			{/* <TableCaption>A list of your recent invoices.</TableCaption> */}
 			<TableHeader>
 				<TableRow>
-					<TableHead className="w-[100px]">Invoice</TableHead>
-					<TableHead>Status</TableHead>
-					<TableHead>Method</TableHead>
-					<TableHead className="text-right">Amount</TableHead>
+					<TableHead>Datoteka</TableHead>
+					<TableHead className="w-[100px]">Autor</TableHead>
+					<TableHead>Oznake</TableHead>
+					<TableHead className="text-right"></TableHead>
 				</TableRow>
 			</TableHeader>
 			<TableBody>
 				{invoices.map((invoice) => (
-					<TableRow key={invoice.invoice}>
-						<TableCell className="font-medium">{invoice.invoice}</TableCell>
-						<TableCell>{invoice.paymentStatus}</TableCell>
-						<TableCell>{invoice.paymentMethod}</TableCell>
-						<TableCell className="text-right">{invoice.totalAmount}</TableCell>
+					<TableRow key={invoice.file}>
+						<TableCell>{invoice.file}</TableCell>
+						<TableCell>{invoice.author}</TableCell>
+						<TableCell>{invoice.tags}</TableCell>
+						<TableCell className="text-right">{invoice.actions}</TableCell>
 					</TableRow>
 				))}
 			</TableBody>
-			<TableFooter>
+			{/* <TableFooter>
 				<TableRow>
 					<TableCell colSpan={3}>Total</TableCell>
 					<TableCell className="text-right">$2,500.00</TableCell>
 				</TableRow>
-			</TableFooter>
+			</TableFooter> */}
 		</Table>
 	);
 };

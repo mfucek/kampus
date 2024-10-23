180296fdfa6b13cd64034481d6080aa07506b2e1
Matija Fućek <matijafucek1@gmail.com>
2024-10-24 00:01:50 +0200

    add basic rbac

---
+++ apps/platform/src/modules/permissions/constants/roles.ts
@@ -0,0 +1,55 @@
+import { Permissions, RoleType } from '@prisma/client';
+
+export const rolePermissions: Record<
+	RoleType,
+	Omit<Permissions, 'accountId' | 'baseRole' | 'account'>
+> = {
+	[RoleType.BASE]: {
+		canEnterPlatform: true,
+		canPost: true,
+		canReply: true,
+		canVote: true,
+		canUploadFiles: true,
+		canUploadImages: true,
+		canChangeTheme: false,
+		canViewAllSummaries: false,
+		canManageTopics: false,
+		canManageColleges: false,
+		canDeleteOthersPosts: false,
+		canDeleteUsers: false,
+		canManageUserRoles: false,
+		canManagePermissions: false
+	},
+	[RoleType.MODERATOR]: {
+		canEnterPlatform: true,
+		canPost: true,
+		canReply: true,
+		canVote: true,
+		canUploadFiles: true,
+		canUploadImages: true,
+		canChangeTheme: true,
+		canViewAllSummaries: true,
+		canManageTopics: true,
+		canManageColleges: true,
+		canDeleteOthersPosts: true,
+		canDeleteUsers: false,
+		canManageUserRoles: false,
+		canManagePermissions: false
+	},
+	[RoleType.ADMIN]: {
+		canEnterPlatform: true,
+		canPost: true,
+		canReply: true,
+		canVote: true,
+		canUploadFiles: true,
+		canUploadImages: true,
+		canChangeTheme: true,
+		canViewAllSummaries: true,
+		canManageTopics: true,
+		canManageColleges: true,
+		canDeleteOthersPosts: true,
+		canDeleteUsers: true,
+		canManageUserRoles: true,
+		canManagePermissions: true
+	}
+};

45c049654e54788cb0f4037c15c2aa3bc5b4caf9
Matija Fućek <matijafucek1@gmail.com>
2024-11-25 11:50:11 +0100

    add floating college cards

--- apps/platform/src/modules/landing-page/pages/landing-page.tsx
+++ apps/platform/src/modules/landing-page/pages/landing-page.tsx
@@ -3,13 +3,15 @@ import { Footer } from '../components/footer';
 import { BenefitsSection } from '../components/section/benefits';
 import { FaqSection } from '../components/section/faq';
 import { FeaturesSection } from '../components/section/features';
-import { HeroSection } from '../components/section/hero';
 import { PricingSection } from '../components/section/pricing';
+import { Test } from '../components/test';
+
 export const LandingPage: FC = async () => {
 	return (
 		<div className="flex flex-col min-h-screen bg-section">
 			<main className="flex-1 bg-section">
-				<HeroSection />
+				<Test />
+				{/* <HeroSection /> */}
 				<FeaturesSection />
 				<BenefitsSection />
 				<PricingSection />

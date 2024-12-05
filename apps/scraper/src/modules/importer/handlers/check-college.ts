7f3406faf85439debc4cfd59f7510f25b638a6d0
Matija Fućek <matijafucek1@gmail.com>
2024-12-05 02:38:02 +0100

    feat: create new college in CLI if slug not found

--- apps/scraper/src/modules/importer/handlers/check-college.ts
+++ apps/scraper/src/modules/importer/handlers/check-college.ts
@@ -5,11 +5,33 @@ import { db } from '@/lib/prisma/db';
 export const checkCollege = async (collegeSlug: string) => {
 	const college = await db.college.findFirst({ where: { slug: collegeSlug } });
 
-	if (!college) {
+	if (college) {
+		p.log.success(`College found: ${college.id}`);
+		return college.id;
+	}
+
+	const shouldCreateCollege = await p.confirm({
+		message: `College not found: ${collegeSlug}. Would you like to create it?`
+	});
+
+	if (!shouldCreateCollege) {
 		throw new Error('College not found.');
 	}
 
-	p.log.success(`College found: ${college.id}`);
+	const name = (await p.text({
+		message: 'College name'
+	})) as string;
 
-	return college.id;
+	const slug = (await p.text({
+		message: 'College slug'
+	})) as string;
+
+	const newCollege = await db.college.create({
+		data: {
+			name,
+			slug
+		}
+	});
+
+	return newCollege.id;
 };

0382b49a0a6c7d408ae46aac4285eee924d1f878
Matija Fućek <matijafucek1@gmail.com>
2025-01-13 20:05:20 +0100

    tweak some more things

--- apps/platform/src/modules/user/components/sections/manage-users.tsx
+++ apps/platform/src/modules/user/components/sections/manage-users.tsx
@@ -3,6 +3,7 @@
 import { type FC, useState } from 'react';
 
 import { Icon } from '@/global/components/icon';
+import { ContentPadding } from '@/global/layouts/content-padding';
 import { Button } from '@/lib/shadcn/ui/button';
 import {
 	Dialog,
@@ -64,45 +65,46 @@ const AccountSection: FC<{ account: ListAccountsItem }> = ({ account }) => {
 	};
 
 	return (
-		<div className="flex flex-col gap-2">
-			<Dialog>
-				<div className="flex flex-row justify-between">
-					{account.user.displayName}
-					<DialogTrigger asChild>
-						<Button variant={'solid-weak'}>
-							<Icon icon="add" />
-							Add
-						</Button>
-					</DialogTrigger>
-				</div>
+		<ContentPadding size="lg">
+			<div className="flex flex-col gap-2">
+				<Dialog>
+					<div className="flex flex-row justify-between">
+						{account.user.displayName}
+						<DialogTrigger asChild>
+							<Button variant={'solid-weak'}>
+								<Icon icon="add" />
+								Add
+							</Button>
+						</DialogTrigger>
+					</div>
 
-				<DialogContent>
-					<DialogHeader className="hidden">
-						<DialogTitle>Add Permission</DialogTitle>
-						<DialogDescription>
-							Add a permission to the account.
-						</DialogDescription>
-					</DialogHeader>
+					<DialogContent>
+						<DialogHeader className="hidden">
+							<DialogTitle>Add Permission</DialogTitle>
+							<DialogDescription>
+								Add a permission to the account.
+							</DialogDescription>
+						</DialogHeader>
 
-					<DialogBody className="p-6 flex flex-col gap-10">
-						<div className="flex flex-row gap-2">
-							<Select
-								value={rule ?? undefined}
-								onValueChange={(value) => setRule(value as RuleType)}
-							>
-								<SelectTrigger className="w-full">
-									<SelectValue placeholder="Select Rule" />
-								</SelectTrigger>
-								<SelectContent>
-									{Object.values(RuleType).map((rule) => (
-										<SelectItem value={rule} key={rule}>
-											{rule}
-										</SelectItem>
-									))}
-								</SelectContent>
-							</Select>
+						<DialogBody className="p-6 flex flex-col gap-10">
+							<div className="flex flex-row gap-2">
+								<Select
+									value={rule ?? undefined}
+									onValueChange={(value) => setRule(value as RuleType)}
+								>
+									<SelectTrigger className="w-full">
+										<SelectValue placeholder="Select Rule" />
+									</SelectTrigger>
+									<SelectContent>
+										{Object.values(RuleType).map((rule) => (
+											<SelectItem value={rule} key={rule}>
+												{rule}
+											</SelectItem>
+										))}
+									</SelectContent>
+								</Select>
 
-							{/* <Select
+								{/* <Select
 								value={scopeType}
 								onValueChange={(value) => setScopeType(value as ScopeType)}
 							>
@@ -114,53 +116,54 @@ const AccountSection: FC<{ account: ListAccountsItem }> = ({ account }) => {
 									<SelectItem value="COLLEGE">College</SelectItem>
 								</SelectContent>
 							</Select> */}
-						</div>
+							</div>
 
-						{/* <Input
+							{/* <Input
 							placeholder="Scope ID"
 							value={scopeId ?? ''}
 							onChange={(e) => setScopeId(e.target.value)}
 						/> */}
 
-						<Select
-							value={scopeId ?? ''}
-							onValueChange={(value) => setScopeId(value)}
-						>
-							<SelectTrigger className="w-full">
-								<SelectValue placeholder="Select Scope" />
-							</SelectTrigger>
-							<SelectContent>
-								{colleges?.map((college) => (
-									<SelectItem value={college.id} key={college.id}>
-										{college.name}
-									</SelectItem>
-								))}
-							</SelectContent>
-						</Select>
+							<Select
+								value={scopeId ?? ''}
+								onValueChange={(value) => setScopeId(value)}
+							>
+								<SelectTrigger className="w-full">
+									<SelectValue placeholder="Select Scope" />
+								</SelectTrigger>
+								<SelectContent>
+									{colleges?.map((college) => (
+										<SelectItem value={college.id} key={college.id}>
+											{college.name}
+										</SelectItem>
+									))}
+								</SelectContent>
+							</Select>
 
-						<Select
-							value={value ? 'true' : 'false'}
-							onValueChange={(value) => setValue(value === 'true')}
-						>
-							<SelectTrigger className="w-full">
-								<SelectValue placeholder="Select Scope" />
-							</SelectTrigger>
-							<SelectContent>
-								<SelectItem value="true">True</SelectItem>
-								<SelectItem value="false">False</SelectItem>
-							</SelectContent>
-						</Select>
+							<Select
+								value={value ? 'true' : 'false'}
+								onValueChange={(value) => setValue(value === 'true')}
+							>
+								<SelectTrigger className="w-full">
+									<SelectValue placeholder="Select Scope" />
+								</SelectTrigger>
+								<SelectContent>
+									<SelectItem value="true">True</SelectItem>
+									<SelectItem value="false">False</SelectItem>
+								</SelectContent>
+							</Select>
 
-						<Button onClick={handleAddPermission}>
-							<Icon icon="add" />
-							Add
-						</Button>
-					</DialogBody>
-				</DialogContent>
-			</Dialog>
+							<Button onClick={handleAddPermission}>
+								<Icon icon="add" />
+								Add
+							</Button>
+						</DialogBody>
+					</DialogContent>
+				</Dialog>
 
-			<PermissionsTable permissions={permissions ?? []} />
-		</div>
+				<PermissionsTable permissions={permissions ?? []} />
+			</div>
+		</ContentPadding>
 	);
 };
 
